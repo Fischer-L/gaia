c290ad6b27deaea393335190fd1bea92edf934f5
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2015-12-09 16:15:39 -0500

    Update the preload hosted apps

---
+++ external-apps/hostedappcache/cache/hostedappcache/js/index.js
@@ -0,0 +1,9 @@
+var body = document.querySelector('#body');
+if (navigator.onLine) {
+  body.classList.add('online');
+  body.innerHTML = 'TV marketplace is online';
+} else {
+  // body.classList.add('offline');
+  // body.innerHTML = 'TV marketplace is offline';
+  location.assign('./offline.html');
+}
