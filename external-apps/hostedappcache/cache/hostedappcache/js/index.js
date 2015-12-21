e33cc08e366d6418c837502f47c7bbb157ba0225
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2015-12-21 18:07:32 +0800

    Remove hostedappcache app and other's cache folders

--- external-apps/hostedappcache/cache/hostedappcache/js/index.js
+++
@@ -1,9 +0,0 @@
-var body = document.querySelector('#body');
-if (navigator.onLine) {
-  body.classList.add('online');
-  body.innerHTML = 'TV marketplace is online';
-} else {
-  // body.classList.add('offline');
-  // body.innerHTML = 'TV marketplace is offline';
-  location.assign('./offline.html');
-}
